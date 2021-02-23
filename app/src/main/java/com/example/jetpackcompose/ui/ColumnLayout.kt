7c31fb640ea4a45872ac4521bab0a57190529636
Armando Ruiz <aranruiz13@gmail.com>
2021-02-23 10:12:23 -0600

    Add Image item to Column childs

--- app/src/main/java/com/example/jetpackcompose/ui/ColumnLayout.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/ColumnLayout.kt
@@ -1,18 +1,25 @@
 package com.example.jetpackcompose.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.R
 
 @Composable
 fun NewsStory() {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null
+        )
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
