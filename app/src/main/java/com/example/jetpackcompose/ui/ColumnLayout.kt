0e2928648657c2c752be46008edb10761783a9fd
Armando Ruiz <aranruiz13@gmail.com>
2021-02-24 09:46:01 -0600

    Add maxLines and overflow attributes to Text title

--- app/src/main/java/com/example/jetpackcompose/ui/ColumnLayout.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/ColumnLayout.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.R
@@ -32,8 +33,12 @@ fun NewsStory() {
             )
             Spacer(modifier = Modifier.height(height = 16.dp))
             Text(
-                "A day in Shark Fin Cove",
-                style = typography.h6
+                "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
             )
             Text(
                 "Davenport, California",
